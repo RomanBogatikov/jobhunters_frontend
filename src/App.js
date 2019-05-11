522d59d434fc50483db4890f938f9f5421dcca8d
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-05-11 11:31:00 -0400

    Connected the front-end to back end. Fetched data and rendering the index page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import React from 'react';
+let baseURL = 'http://localhost:3003'
+// JUST TO RENDER THE DATA, I ADDED LOCALHOST:3003. WE CAN UPDATE TO THE BUILD PACK LATER ON
+
 
 class App extends React.Component {
+  // ADDED CONSTRUCTOR
+  constructor(props) {
+    super(props)
+    this.state = {
+      jobs: []
+    }
+    this.getJobs = this.getJobs.bind(this)
+  }
+
+  componentDidMount(){
+    this.getJobs()
+  }
+  
+  getJobs() {
+    fetch(baseURL+ '/jobs')
+      .then(data => {
+        return data.json()},
+        err => console.log(err))
+      .then(parsedData => this.setState({jobs: parsedData}),
+       err=> console.log(err))
+
+       console.log('current base URL:', baseURL)
+
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <h1>This is the start of the frontend!</h1>
+        <table>
+  <tbody>
+    { this.state.jobs.map(jobs => {
+        return (
+          <tr key={jobs._id} >
+            <td> {jobs.agency }</td>
+            <td> {jobs.business_title }</td>
+            <td> {jobs.job_description }</td>
+          </tr>
+        )
+      })
+    }
+  </tbody>
+</table>
       </div>
     )
   }
