f96a2977be678a56d099cd4ee078e5be2d610ce4
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-05-13 12:29:00 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 let baseURL = 'http://localhost:3003'
 // JUST TO RENDER THE DATA, I ADDED LOCALHOST:3003 TO BASEURL. WE CAN UPDATE TO THE BUILD PACK LATER ON.
 
-
 class App extends React.Component {
   // ADDED CONSTRUCTOR
   constructor(props) {
@@ -13,19 +12,20 @@ class App extends React.Component {
     this.getJobs = this.getJobs.bind(this)
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getJobs()
   }
-  
-  getJobs() {
-    fetch(baseURL+ '/jobs')
-      .then(data => {
-        return data.json()},
-        err => console.log(err))
-      .then(parsedData => this.setState({jobs: parsedData}),
-       err=> console.log(err))
 
-       console.log('current base URL:', baseURL)
+  getJobs() {
+    fetch(baseURL + '/jobs')
+      .then(data => {
+        return data.json()
+      },
+        err => console.log(err))
+      .then(parsedData => this.setState({ jobs: parsedData }),
+        err => console.log(err))
+
+    console.log('current base URL:', baseURL)
 
   }
 
@@ -34,18 +34,18 @@ class App extends React.Component {
       <div className="container">
         <h1>This is the start of the frontend!</h1>
         <table>
-  <tbody>
-    { this.state.jobs.map(jobs => {
-        return (
-          <tr key={jobs._id} >
-            <td> {jobs.agency }</td>
-            <td> {jobs.business_title }</td>
-          </tr>
-        )
-      })
-    }
-  </tbody>
-</table>
+          <tbody>
+            {this.state.jobs.map(jobs => {
+              return (
+                <tr key={jobs._id} >
+                  <td> {jobs.agency}</td>
+                  <td> {jobs.business_title}</td>
+                </tr>
+              )
+            })
+            }
+          </tbody>
+        </table>
       </div>
     )
   }
